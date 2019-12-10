2ebdb7c1f8ab6ae9aa4fb62470f0ae94c37356c9
sm <samir.m8@gmail.com>
2019-12-10 15:57:23 -0500

    finished p1, cleaned up code

--- 01 - rocket-equation/JS/main.js
+++ 01 - rocket-equation/JS/main.js
@@ -1,36 +1,40 @@
-// import { readFile } from 'fs';
 const fs = require("fs");
 
-function fuelCounterUpper(mass) {
-    let fuel = Math.floor(mass/3);
-    fuel = fuel - 2;
-    // console.log(fuel);
-    return fuel;
+(async () => {
+    const masses = await readFile('input.txt');
+    const total = process(masses);
+    console.log(total);
+})();
+
+function fuelCounterUpper(moduleMass) {
+    let fuelMass = Math.floor(moduleMass / 3);
+    fuelMass = fuelMass - 2;
+    if (fuelMass <= 6) return fuelMass;
+    return fuelMass + fuelCounterUpper(fuelMass);
 }
 
 function validateInput(input) {
     if (isNaN(input)) {
         console.log('encountered not a number in file');
+        exit();
     } else {
         return input;
     }
 }
 
-    let masses = [];
-
-    fs.readFile('masses.txt', 'utf8', (err, data) => {
-        if (err) console.log(err);
-        else { 
-            // console.log(data); 
-            masses = data.trim().split('\n');
-            // console.log(masses);
-            process(masses);
-        }
+async function readFile(path) {
+    return new Promise((resolve, reject) => {
+        return fs.readFile(path, 'utf8', (err, data) => {
+            if (err) reject(err);
+            else {
+                resolve(data.trim().split('\n'));
+            }
+        });
     });
+}
 
-    function process(masses) {
-        const total = masses.map(mass => validateInput(mass))
-            .map(mass => fuelCounterUpper(mass))
-            .reduce((a, b) => { return a + b }, 0);
-        console.log(total);
-    }
+function process(masses) {
+    return masses.map(mass => validateInput(mass))
+        .map(mass => fuelCounterUpper(mass))
+        .reduce((a, b) => { return a + b }, 0);
+}
